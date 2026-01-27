f29f416dc4121291afd74dd7c61599b6942a4c51
Suhaas15 <suhaas15032001@gmail.com>
2026-01-26 19:50:00 -0800

    results page UI

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,5 @@
+// This is a new Dart file
+
+void main() {
+  print("Hello, World!");
+}
---
+++ lib/screens/results_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+import 'package:bmi_calculator/components/bottomButton.dart';
+
+class ResultsPage extends StatelessWidget {
+  const ResultsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        centerTitle: true, // This centers the text correctly
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text('Result', style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text('Normal', style: kResultTextStyle),
+                  Text('18.3', style: kBMITextStyle),
+                  Text(
+                    'Your BMI is great! Eat more meat!!',
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            buttonTitle: "RE-CALCULATE",
+          ),
+        ],
+      ),
+    );
+  }
+}
